d1d1c88381ec7c0b166c7255b39861ce332b21b6
ThanhAnNguyen2014 <kieuquanh94@gmail.com>
2016-09-25 22:41:40 +0700

    intial check in of sales tool.

---
+++ GitAndEclips/src/mypack/Main.java
@@ -0,0 +1,20 @@
+package mypack;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		SalesData data= new SalesData();
+		
+		displayGreeting();
+		data.display();
+	}
+	private static void displayGreeting() {
+		System.out.println("HELLO HAPPY SALES PEOPLE!");
+		System.out.println("THIS APP SHOW DATA!");
+		
+		
+	}
+
+}
+  
