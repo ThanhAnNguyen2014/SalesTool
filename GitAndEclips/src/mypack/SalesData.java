e355d5eccc5fa3fde612f41ba058db220b27f6f1
ThanhAnNguyen2014 <kieuquanh94@gmail.com>
2016-09-25 23:03:09 +0700

    Add output data 2

--- GitAndEclips/src/mypack/SalesData.java
+++ GitAndEclips/src/mypack/SalesData.java
@@ -4,5 +4,6 @@ public class SalesData {
 	int data[]={0,4,42};
 	public void display() {
 		System.out.println("Todo: Implement data display");
+		System.out.println("Todo: Implement data display 2");
 	}
 }
